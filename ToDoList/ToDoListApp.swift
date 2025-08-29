e1e4a340e5603cc505d1f495a8749ce9c436e344
Pedro Flores <pedro_23_96@hotmail.com>
2025-08-29 16:20:34 +0200

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Pedro Manuel Flores Crespo on 29/8/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
